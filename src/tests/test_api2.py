e1a12aa354997b9d6a80d0734d4e42d9726ec660
kalpak04 <kalpak2404@gmail.com>
2024-12-30 19:31:02 +0530

    added some files

---
+++ src/tests/test_api2.py
@@ -0,0 +1,20 @@
+import requests
+
+def test_api():
+    
+
+    url = 'https://reqres.in/api/users?page=2'
+    data = { 
+            "first_name" : "Kalpak",
+            "last_name" : "Pimpale"
+         }
+
+    response = requests.post(url, json=data)
+
+
+    return response.text
+
+output = test_api()
+
+assert "first_name" in output == "Kalpak"
+
